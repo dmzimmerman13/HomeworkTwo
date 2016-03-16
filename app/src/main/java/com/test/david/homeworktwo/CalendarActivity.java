35112888aab2b4922c48b8e5dee26f3047f2c98b
dmzimmerman13 <fondyzim@hotmail.com>
2016-03-15 21:22:12 -0500

    Added Fragment Implementation in the Calendar Activity. Moved the Date Picker to that fragment as well.

--- app/src/main/java/com/test/david/homeworktwo/CalendarActivity.java
+++ app/src/main/java/com/test/david/homeworktwo/CalendarActivity.java
@@ -1,13 +1,25 @@
 package com.test.david.homeworktwo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity implements
+        CalendarFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, CalendarFragment.newInstance(null, null))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
