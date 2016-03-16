e365c65628e2a498a39b9101d6da3b01c1a66a11
dmzimmerman13 <fondyzim@hotmail.com>
2016-03-15 22:06:17 -0500

    ViewEvent Button now creates a new fragment when called passing the date chosen from the DatePicker

--- app/src/main/java/com/test/david/homeworktwo/EventFragment.java
+++ app/src/main/java/com/test/david/homeworktwo/EventFragment.java
@@ -7,19 +7,17 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.DatePicker;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link CalendarFragment.OnFragmentInteractionListener} interface
+ * {@link EventFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link CalendarFragment#newInstance} factory method to
+ * Use the {@link EventFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalendarFragment extends Fragment {
+public class EventFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,12 +27,9 @@ public class CalendarFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Button viewEvent;
-    private DatePicker datePicker;
-
     private OnFragmentInteractionListener mListener;
 
-    public CalendarFragment() {
+    public EventFragment() {
         // Required empty public constructor
     }
 
@@ -44,11 +39,11 @@ public class CalendarFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
+     * @return A new instance of fragment EventFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
+    public static EventFragment newInstance(String param1, String param2) {
+        EventFragment fragment = new EventFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,32 +64,14 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-
-        viewEvent = (Button)view.findViewById(R.id.viewEvent);
-        datePicker = (DatePicker)view.findViewById(R.id.datePicker);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_event, container, false);
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-
-        //question answer
-        viewEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String header = datePicker.getMonth() + " " + datePicker.getDayOfMonth() + " " + datePicker.getYear();
-                getFragmentManager()
-                        .beginTransaction()
-                        .addToBackStack(null)
-                        .replace(R.id.main_fragment_container, EventFragment.newInstance(header, null))
-                        .commit();
-            }
-        });
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
     @Override
@@ -125,6 +102,7 @@ public class CalendarFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
